33609f11911261e1a923314f5568b23cf3b9a3e3
Abinu <asivabalan@spartaglobal.com>
2018-12-05 13:28:40 +0000

    running ruby file tests for application form

--- selenium/selenium_walkthrough/selenium_walkthrough.rb
+++ selenium/selenium_walkthrough/selenium_walkthrough.rb
@@ -10,25 +10,14 @@ class SeleniumQAToolsForm
     @gender_male_field_name = 'sex-0'
     @gender_female_field_name = 'sex-1'
     @years_of_experince_0 = 'exp-0'
-    @years_of_experince_1 = 'exp-1'
-    @years_of_experince_2 = 'exp-2'
-    @years_of_experince_3 = 'exp-3'
-    @years_of_experince_4 = 'exp-4'
-    @years_of_experince_5 = 'exp-5'
-    @years_of_experince_6 = 'exp-6'
-    @years_of_experince_7 = 'exp-7'
     @date_field = 'datepicker'
     @profession_manual_tester = 'profession-0'
     @profession_automated_tester = 'profession-1'
-    @download_selenium_framework =
-    'http://toolsqa.wpengine.com/wp-content/uploads/2014/04/OnlineStore.zip'
-    @download_test_file = 'http://20tvni1sjxyh352kld2lslvc.wpengine.netdna-cdn.com/wp-content/uploads/2016/09/Test-File-to-Download.xlsx'
     @automation_tool_QTP = 'tool-0'
     @automation_tool_seleniumIDE = 'tool-1'
     @automation_tool_senenium_webdriver = 'tool-2'
     @dropdown_continents = 'continents'
-
-
+    @dropdown_commands = 'selenium_commands'
   end
 
   def visit_practice_form
@@ -40,10 +29,71 @@ class SeleniumQAToolsForm
   end
 
   def input_lastname_field(text)
+    @chrome_driver.find_element(:name, @last_name_field_name).send_keys(text)
+  end
 
+  def input_gender_male_field
+    @chrome_driver.find_element(:id, @gender_male_field_name).click
+  end
+
+  def input_gender_female_field
+    @chrome_driver.find_element(:id, @gender_female_field_name).click
+  end
+
+  def input_years_of_experience_0
+    @chrome_driver.find_element(:id, @years_of_experince_0).click
+  end
+
+  def input_date(text)
+    @chrome_driver.find_element(:id, @date_field).send_keys(text)
+  end
+
+  def input_profession_manual_tester
+    @chrome_driver.find_element(:id, @profession_manual_tester).click
+  end
+
+  def input_profession_automated_tester
+    @chrome_driver.find_element(:id, @profession_automated_tester).click
+  end
+
+  def input_automation_tool_QTP
+    @chrome_driver.find_element(:id, @automation_tool_QTP).click
+  end
+
+  def input_automation_tool_seleniumIDE
+    @chrome_driver.find_element(:id, @automation_tool_seleniumIDE).click
+  end
+
+  def input_automation_tool_webdriver
+    @chrome_driver.find_element(:id, @automation_tool_senenium_webdriver).click
+  end
+
+  def input_continent
+    dropDownMenu = @chrome_driver.find_element(:id, @dropdown_continents)
+    option = Selenium::WebDriver::Support::Select.new(dropDownMenu)
+    option.select_by(:text, 'Europe')
+  end
+
+  def input_selenium_commands
+    dropDown = @chrome_driver.find_element(:id, @dropdown_commands)
+    option = Selenium::WebDriver::Support::Select.new(dropDown)
+    option.select_by(:text, 'Navigation Commands')
+  end
 end
 
 test = SeleniumQAToolsForm.new
 test.visit_practice_form
-test.input_firstname_field('Richard')
+test.input_firstname_field('Abinu')
+test.input_lastname_field('Sivabalan')
+test.input_gender_male_field
+test.input_gender_female_field
+test.input_years_of_experience_0
+test.input_date('10-12-2016')
+test.input_profession_manual_tester
+test.input_profession_automated_tester
+test.input_automation_tool_QTP
+test.input_automation_tool_seleniumIDE
+test.input_automation_tool_webdriver
+test.input_continent
+test.input_selenium_commands
 sleep 5
